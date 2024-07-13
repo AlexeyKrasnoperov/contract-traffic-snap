3baf72045ab9522363dafaffc28075f796c47a01
Alexey Krasnoperov <Alexey.Krasnoperov@gmail.com>
2024-07-13 23:39:35 +0200

    --amend

--- packages/backend/scripts/deploy.js
+++ packages/backend/scripts/deploy.js
@@ -1,16 +1,16 @@
 const { ethers } = require('hardhat');
 
 async function main() {
-  const DomainOwnership = await ethers.getContractFactory('SafeCity');
-  const domainOwnership = await DomainOwnership.deploy(
+  const SafeCity = await ethers.getContractFactory('SafeCity');
+  const safeCity = await SafeCity.deploy(
     // Sepolia
     "0xb83E47C2bC239B3bf370bc41e1459A34b41238D0", // router address - Check to get the router address for your supported network https://docs.chain.link/chainlink-functions/supported-networks
     "0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000", // donID - Check to get the donID for your supported network https://docs.chain.link/chainlink-functions/supported-networks
     3107 // subscription ID
   );
-  await domainOwnership.waitForDeployment();
+  await safeCity.waitForDeployment();
 
-  console.log("DomainOwnership deployed to:", await domainOwnership.getAddress());
+  console.log("SafeCity deployed to:", await safeCity.getAddress());
 }
 
 main()
